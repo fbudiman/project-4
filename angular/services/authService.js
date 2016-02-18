7e2d0597dc99aa16c3b808fe86c5b3c15195efc1
Fran Budiman <fbudiman14@hotmail.com>
2016-02-17 19:27:11 -0800

    adding auth interceptor factory

--- angular/services/authService.js
+++ angular/services/authService.js
@@ -73,6 +73,31 @@ angular.module('authService', [])
 
     var interceptorFactory = {};
 
+    // this will happen on all HTTP requests
+    interceptorFactory.request = function(config) {
+
+      // grab the token
+      var token = AuthToken.getToken();
+
+      // if the token exists, add it to the header as x-access-token
+      if (token)
+        config.headers['x-access-token'] = token;
+
+      return config;
+    };
+
+    // happens on response errors
+    interceptorFactory.responseError = function(response) {
+
+       // if our server returns a 403 forbidden response
+      if (response.status == 403) {
+        AuthToken.setToken();
+        $location.path('/login');
+      }
+
+       return $q.reject(response);
+    };
+
     return interceptorFactory;
 
   });
