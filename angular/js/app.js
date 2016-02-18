eb45ea879e22f1ffaeca4d291700559b42f931d6
Fran Budiman <fbudiman14@hotmail.com>
2016-02-18 14:07:49 -0800

    adding ng-if for login/logout functions

--- angular/js/app.js
+++ angular/js/app.js
@@ -12,44 +12,62 @@ function MainRouter($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('newsfeed', {
       url: '/',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/newsfeed.html'
     })
     .state('search', {
       url: '/search',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/search.html'
     })
     .state('about', {
       url: '/about',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/about.html'
     })
     .state('user', {
       url: '/user',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/user.html'
     })
     .state('login', {
       url: '/login',
-      controller : 'MainController',
-      controllerAs: 'login',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/login.html'
     })
     .state('register', {
       url: '/register',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/register.html'
     })
     .state('all', {
       url: '/all',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/all.html'
     })
     .state('adoptions', {
       url: '/adoptions',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/adoptions.html'
     })
     .state('lost', {
       url: '/lost',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/lost.html'
     })
     .state('misc', {
       url: '/misc',
+      controller: 'MainController',
+      controllerAs: 'main',
       templateUrl: 'views/misc.html'
     })
 
